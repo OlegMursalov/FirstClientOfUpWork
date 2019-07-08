33960625031f2cd079908e6695b9ed019661d2f5
Oleg Mursalov <olofovich@mail.ru>
2019-07-09 00:35:49 +0300

    MSI FIXES

---
+++ ScriptWPF/CustomActionForWix/CustomAction.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomActionForWix
+{
+    public class CustomAction
+    {
+        [CustomAction]
+        public static ActionResult CustomAction1(Session session)
+        {
+            session["MSGVAR"] = "Some Message";
+            return ActionResult.Success;
+        }
+    }
+}
