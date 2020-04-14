4a2efc2f138d1c3b12c820563af0491d5e4262ce
Oleg Mursalov <olofovich@mail.ru>
2020-04-14 16:07:44 +0300

    file config.php используется на FTP сервере как includeный файл в других php файлах, которые лезут в MySQL.
    думаю сейчас сюда же вписать FTP учетные данные.

--- Site/config.php
+++ Site/config.php
@@ -2,7 +2,7 @@
 	$config = array(
 	'DB_HOST' => 'localhost',
 	'DB_USERNAME' => 'd02f240b',
-	'DB_PASSWORD' => 'BvLnSTHEzyBxRo38',
+	'DB_PASSWORD' => 'YRQUG57n5CHDX4kL',
 	'DB_DATABASE' => 'd02f240b'
 );
 ?>
